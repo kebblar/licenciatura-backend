3e9b2d2a45d1372f4963da03d40189a702e169b0
mentesniker <mentesniker@gmail.com>
2022-02-21 21:20:47 -0600

    feature: se agrego el codigo inicial con un controlador que contesta con tu nombre

---
+++ src/main/java/unam/mx/backend/BackendApplication.java
@@ -0,0 +1,13 @@
+package unam.mx.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
