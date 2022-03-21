6460c147d345ea687d200d5198b83f3f5d2a7b17
mentesniker <mentesniker@gmail.com>
2022-03-21 17:23:04 -0600

    docs: se agrego la documentacion del servicio de autenticacion

--- src/main/java/mx/unam/backend/utils/AuthenticatorService.java
+++ src/main/java/mx/unam/backend/utils/AuthenticatorService.java
@@ -1,8 +1,36 @@
+/*
+ *
+ * Paquete:     mx.unam.backend.model
+ * Proyecto:    licenciatura-backend
+ * Tipo:        interface
+ * Nombre:      AuthenticatorService
+ * Autor:       Luis Martinez 
+ * Versión:     1.0-SNAPSHOT
+ *
+ * Historia:
+ *              Creación: 21 Mar 2022
+ */
 package mx.unam.backend.utils;
 
 import mx.unam.backend.exceptions.ServiceException;
 
+/**
+ * <p>Definición de la interfaz de servicios para Authenticacion.
+ *
+ * @author  mentesniker
+ * @version 1.0-SNAPSHOT
+ * @since   1.0-SNAPSHOT
+ */
 public interface AuthenticatorService {
 
+    /**
+     * Revisa si el correo contenido en el jwt recibido
+     * tiene el rol especificado por el primer parametro.
+     *
+     * @param rolObjetivo String con el rol que deberia de tener 
+     * el usuario para acceder al recurso.
+     * @param jwt String con el token del usuario.
+     * @throws ServiceException if any
+     */
     public void authentica(String rolObjetivo, String jwt) throws ServiceException;
 }
--- src/main/java/mx/unam/backend/utils/AuthenticatorServiceImpl.java
+++ src/main/java/mx/unam/backend/utils/AuthenticatorServiceImpl.java
@@ -1,3 +1,15 @@
+/*
+ *
+ * Paquete:     mx.unam.backend.model
+ * Proyecto:    licenciatura-backend
+ * Tipo:        Class
+ * Nombre:      AuthenticatorServiceImpl
+ * Autor:       Luis Martinez 
+ * Versión:     1.0-SNAPSHOT
+ *
+ * Historia:
+ *              Creación: 21 Mar 2022
+ */
 package mx.unam.backend.utils;
 
 import java.util.List;
@@ -9,7 +21,17 @@ import mx.unam.backend.exceptions.CustomException;
 import mx.unam.backend.exceptions.ServiceException;
 import mx.unam.backend.mapper.UsuarioMapper;
 import mx.unam.backend.model.Rol;
-
+/**
+ * <p>Implementación de la interfaz {@link mx.unam.backend.service.AuthenticatorService}.
+ *
+ * <p>Todos los métodos de esta clase disparan {@link mx.unam.backend.exceptions.ServiceException}
+ *
+ * @author  mentesniker
+ * @see     unam.mx.backend.domain.Usuario
+ * @see     mx.unam.backend.service.UsuarioService
+ * @version 1.0-SNAPSHOT
+ * @since   1.0-SNAPSHOT
+ */
 @Service
 public class AuthenticatorServiceImpl implements AuthenticatorService{
     private UsuarioMapper usuarioMapper;
@@ -21,7 +43,7 @@ public class AuthenticatorServiceImpl implements AuthenticatorService{
         this.jwtUtilInstance = JWTUtil.getInstance();
         this.usuarioMapper=usuarioMapper;
     }
-
+    /** {@inheritDoc} */
     public void authentica(String rolObjetivo, String jwt) throws ServiceException{
         String correo = this.jwtUtilInstance.getCorreoFromJwt(jwt, this.encryptKey);
         List<Rol> rolesUsuario = usuarioMapper.getRoles(correo);
