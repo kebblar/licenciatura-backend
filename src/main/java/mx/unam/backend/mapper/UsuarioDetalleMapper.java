9ae8500ab154d3630bd822a14a582f389b377624
SantiagoArroyoL <santiagoarroyo@ciencias.unam.mx>
2022-06-05 00:30:34 -0500

    DetalleUsuario

--- src/main/java/mx/unam/backend/mapper/UsuarioDetalleMapper.java
+++ src/main/java/mx/unam/backend/mapper/UsuarioDetalleMapper.java
@@ -24,29 +24,67 @@ package mx.unam.backend.mapper;
 import java.sql.SQLException;
 
 import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.ResultMap;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.exceptions.PersistenceException;
 import org.springframework.stereotype.Repository;
+
 import mx.unam.backend.model.UsuarioDetalle;
 
 /**
- * <p>Descripción:</p>
- * Interface 'Mapper' MyBatis asociado a la entidad {@link io.kebblar.petstore.api.model.domain.UsuarioDetalle}.
+ * <p>
+ * Descripción:
+ * </p>
+ * Interface 'Mapper' MyBatis asociado a la entidad
+ * {@link io.kebblar.petstore.api.model.domain.UsuarioDetalle}.
  *
- * @author  garellano
- * @see     io.kebblar.petstore.api.model.domain.UsuarioDetalle
+ * @author garellano
+ * @see io.kebblar.petstore.api.model.domain.UsuarioDetalle
  * @version 1.0-SNAPSHOT
- * @since   1.0-SNAPSHOT
+ * @since 1.0-SNAPSHOT
  */
 @Repository
 public interface UsuarioDetalleMapper {
 
     /**
-     * Inserta un objeto de tipo {@link io.kebblar.petstore.api.model.domain.UsuarioDetalle}  con base en la información dada por el objeto de tipo 'UsuarioDetalle'.
+     * Inserta un objeto de tipo
+     * {@link io.kebblar.petstore.api.model.domain.UsuarioDetalle} con base en la
+     * información dada por el objeto de tipo 'UsuarioDetalle'.
      *
-     * @param usuarioDetalle objeto de tipo {@link io.kebblar.petstore.api.model.domain.UsuarioDetalle} a ser insertado.
+     * @param usuarioDetalle objeto de tipo
+     *                       {@link io.kebblar.petstore.api.model.domain.UsuarioDetalle}
+     *                       a ser insertado.
      * @return el auto incremental asociado a esa inserción.
-     * @throws java.sql.SQLException Se dispara en caso de que se dispare un error en esta operación desde la base de datos.
+     * @throws java.sql.SQLException Se dispara en caso de que se dispare un error
+     *                               en esta operación desde la base de datos.
      */
     @Insert("INSERT INTO usuarioDetalle(idUsuario, nombre, segundoApellido, primerApellido, nickName, fechaNacimiento, telefonoCelular) VALUES(#{id}, #{nombre}, #{segundoApellido}, #{primerApellido}, #{nickName}, #{fechaNacimiento}, #{telefonoCelular} )")
     Integer insert(UsuarioDetalle usuarioDetalle) throws SQLException;
+
+    /**
+     * Busca un objeto de tipo '{@link mx.unam.backend.model.Publicacion} '
+     * contenido en
+     * la base de datos usando su id.
+     *
+     * @param publicacionId el id de la publicacion a buscar.
+     * @return un objeto de tipo '{@link mx.unam.backend.model.Publicacion} '.
+     * @throws java.sql.PersistenceException Se dispara en caso de que se dispare un
+     *                                       error en esta operación desde la base
+     *                                       de datos.
+     **/
+    @Results(id = "UsuarioDetalleMap", value = {
+            @Result(property = "id", column = "idUsuario"),
+            @Result(property = "nombre", column = "nombre"),
+            @Result(property = "primerApellido", column = "primerApellido"),
+            @Result(property = "segundoApellido", column = "segundoApellido"),
+            @Result(property = "nickName", column = "nickName"),
+            @Result(property = "fechaNacimiento", column = "fechaNacimiento"),
+            @Result(property = "telefonoCelular", column = "telefonoCelular") })
+    @ResultMap("PublicacionMap")
+    @Select("SELECT * FROM usuarioDetalle WHERE usuario_id = #{usuario_id};")
+    UsuarioDetalle getByUsuarioId(@Param("usuarioId") int usuarioId) throws PersistenceException;
 
 }
--- src/main/java/mx/unam/backend/rest/UsuarioController.java
+++ src/main/java/mx/unam/backend/rest/UsuarioController.java
@@ -12,6 +12,7 @@
  */
 package mx.unam.backend.rest;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,107 +21,93 @@ import org.springframework.web.bind.annotation.RequestBody;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import mx.unam.backend.exceptions.ControllerException;
+import mx.unam.backend.exceptions.ServiceException;
 import mx.unam.backend.model.CredencialesRequest;
 import mx.unam.backend.model.Login;
 import mx.unam.backend.model.Preregistro;
 import mx.unam.backend.model.RecuperacionTokenRequest;
 import mx.unam.backend.model.Usuario;
+import mx.unam.backend.model.UsuarioDetalle;
 import mx.unam.backend.service.UsuarioService;
 
 /**
- * Implementacion  del controlador REST asociado a los endpoints
+ * Implementacion del controlador REST asociado a los endpoints
  * de gestión del AccessController.
  *
- * <p>Todos los métodos de esta clase disparan {@link ControllerException}</p>
+ * <p>
+ * Todos los métodos de esta clase disparan {@link ControllerException}
+ * </p>
  *
- * <p>NOTA IMPORTANTE: Los  distintos métodos de este controlador no
- * llevan  javadoc  debido a que la  documentación  Swagger  API
- * cumple con ese objetivo.</p>
+ * <p>
+ * NOTA IMPORTANTE: Los distintos métodos de este controlador no
+ * llevan javadoc debido a que la documentación Swagger API
+ * cumple con ese objetivo.
+ * </p>
  *
- * @author  mentesniker
- * @see     mx.unam.backend.model.CredencialesRequest
- * @see     mx.unam.backend.model.RecuperacionTokenRequest
- * @see     mx.unam.backend.service.UsuarioService
+ * @author mentesniker
+ * @see mx.unam.backend.model.CredencialesRequest
+ * @see mx.unam.backend.model.RecuperacionTokenRequest
+ * @see mx.unam.backend.service.UsuarioService
  * @version 1.0-SNAPSHOT
- * @since   1.0-SNAPSHOT
+ * @since 1.0-SNAPSHOT
  */
 @RestController
 @RequestMapping(value = "/usuario")
 public class UsuarioController {
     private final UsuarioService usuarioService;
 
-     /**
+    /**
      * Constructor que realiza el setting de los servicios que serán
      * utilizados en este controlador.
      *
      * @param usuarioService Servicios de AccessService
      */
-    public UsuarioController(UsuarioService usuarioService){
+    public UsuarioController(UsuarioService usuarioService) {
         this.usuarioService = usuarioService;
     }
 
-    @ApiOperation(
-            value = "UsuarioController::login",
-            notes = "Recibe el usuario con el que queremos iniciar sesion.")
-    @PostMapping(
-        path = "/login",
-        produces = "application/json; charset=utf-8")
-    public Login login(@ApiParam(
-        name = "usuario",
-        value = "Representa a un usuario (mail, password)")
-        @RequestBody CredencialesRequest usuario) throws ControllerException{
-        return usuarioService.login(usuario); 
+    @ApiOperation(value = "UsuarioController::login", notes = "Recibe el usuario con el que queremos iniciar sesion.")
+    @PostMapping(path = "/login", produces = "application/json; charset=utf-8")
+    public Login login(
+            @ApiParam(name = "usuario", value = "Representa a un usuario (mail, password)") @RequestBody CredencialesRequest usuario)
+            throws ControllerException {
+        return usuarioService.login(usuario);
     }
 
-    @ApiOperation(
-            value = "UsuarioController::recuperarClave",
-            notes = "Recibe el mail del usuario del que queremos recuperar su contrasena.")
-    @PostMapping(
-        path = "/recupera-clave",
-        produces = "application/json; charset=utf-8")
-    public Usuario recuperarClave(@ApiParam(
-        name = "mail",
-        value = "Contiene el correo del usuario")
-        @RequestBody String mail) throws ControllerException{
-        return usuarioService.solicitaRegeneracionClave(mail); 
+    @ApiOperation(value = "UsuarioController::recuperarClave", notes = "Recibe el mail del usuario del que queremos recuperar su contrasena.")
+    @PostMapping(path = "/recupera-clave", produces = "application/json; charset=utf-8")
+    public Usuario recuperarClave(
+            @ApiParam(name = "mail", value = "Contiene el correo del usuario") @RequestBody String mail)
+            throws ControllerException {
+        return usuarioService.solicitaRegeneracionClave(mail);
     }
 
-    @ApiOperation(
-            value = "UsuarioController::confirmaRegeneracionClave",
-            notes = "Recibe la nueva clave y el token del usuario del que queremos recuperar su contrasena.")
-    @PostMapping(
-        path = "/confirma-token",
-        produces = "application/json; charset=utf-8")
-    public Usuario confirmaRegeneraClave(@ApiParam(
-        name = "clave nueva y Token",
-        value = "Representa a una confirmacion de regeneracion (mail, nueva clave y token)")
-        @RequestBody RecuperacionTokenRequest tokenRequest) throws ControllerException{
-        return usuarioService.confirmaRegeneraClave(tokenRequest); 
+    @ApiOperation(value = "UsuarioController::confirmaRegeneracionClave", notes = "Recibe la nueva clave y el token del usuario del que queremos recuperar su contrasena.")
+    @PostMapping(path = "/confirma-token", produces = "application/json; charset=utf-8")
+    public Usuario confirmaRegeneraClave(
+            @ApiParam(name = "clave nueva y Token", value = "Representa a una confirmacion de regeneracion (mail, nueva clave y token)") @RequestBody RecuperacionTokenRequest tokenRequest)
+            throws ControllerException {
+        return usuarioService.confirmaRegeneraClave(tokenRequest);
     }
 
-    @ApiOperation(
-            value = "UsuarioController::preRegistro",
-            notes = "Recibe la informacion del usuario necesaria para su registro")
-    @PostMapping(
-        path = "/preregistro",
-        produces = "application/json; charset=utf-8")
-    public Preregistro preRegistro(@ApiParam(
-        name = "preregistro",
-        value = "Contiene la informacion que un usuario necesita para hacer un preregistro.")
-        @RequestBody Preregistro preregistro) throws ControllerException{
-        return usuarioService.preRegistro(preregistro); 
+    @ApiOperation(value = "UsuarioController::preRegistro", notes = "Recibe la informacion del usuario necesaria para su registro")
+    @PostMapping(path = "/preregistro", produces = "application/json; charset=utf-8")
+    public Preregistro preRegistro(
+            @ApiParam(name = "preregistro", value = "Contiene la informacion que un usuario necesita para hacer un preregistro.") @RequestBody Preregistro preregistro)
+            throws ControllerException {
+        return usuarioService.preRegistro(preregistro);
     }
 
-    @ApiOperation(
-            value = "UsuarioController::preRegistro",
-            notes = "Recibe el token enviado al correo electronico del usuario")
-    @PostMapping(
-        path = "/confirma-registro",
-        produces = "application/json; charset=utf-8")
-    public Usuario confirmaPreregistro(@ApiParam(
-        name = "token",
-        value = "Contiene el token de confirmacion.")
-        @RequestBody String token) throws ControllerException{
-        return usuarioService.confirmaPreregistro(token); 
+    @ApiOperation(value = "UsuarioController::preRegistro", notes = "Recibe el token enviado al correo electronico del usuario")
+    @PostMapping(path = "/confirma-registro", produces = "application/json; charset=utf-8")
+    public Usuario confirmaPreregistro(
+            @ApiParam(name = "token", value = "Contiene el token de confirmacion.") @RequestBody String token)
+            throws ControllerException {
+        return usuarioService.confirmaPreregistro(token);
+    }
+
+    @GetMapping(path = "/detalle", produces = "application/json; charset=utf-8")
+    public UsuarioDetalle getPublicacion(int usuarioId) throws ServiceException {
+        return usuarioService.solicitarUsuario(usuarioId);
     }
 }
